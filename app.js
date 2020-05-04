d7c4067803cf3fbe64caeb5145f95d1dfb523051
Ross Hazelett <rhhazelett@gmail.com>
2020-05-04 18:04:51 -0400

    dno

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
-
 function onSiteLoad() {
-  $('.modal-div').hide();
+  $(".modal-div").hide();
 }
 
 function closeModal() {
-  $('.modal-div').hide();
+  $(".modal-div").hide();
 }
 
 function openMobileNav() {
-  event.preventDefault()
-  document.getElementById('navbar-mobile-content').style.width = '50vw';
+  event.preventDefault();
+  document.getElementById("navbar-mobile-content").style.width = "50vw";
 }
 
 function closeMobileNav() {
-  event.preventDefault()
-  document.getElementById('navbar-mobile-content').style.width = '0';
+  event.preventDefault();
+  document.getElementById("navbar-mobile-content").style.width = "0";
 }
 ////////////////////
 // Project Modals //
 ////////////////////
 function showPathfinderModal() {
-  $('#pathfinder-modal').show();
+  $("#pathfinder-modal").show();
 }
 
 function showWooferModal() {
-  $('#woofer-modal').show();
+  $("#woofer-modal").show();
 }
 
 function showFacedetectModal() {
-  $('#facedetect-modal').show();
+  $("#facedetect-modal").show();
 }
 
 function showCurioModal() {
-  $('#curio-modal').show();
+  $("#curio-modal").show();
 }
 
 function showTictactoeModal() {
-  $('#tictactoe-modal').show();
-
+  $("#tictactoe-modal").show();
 }
 
 // SCROLLING //
@@ -45,103 +43,107 @@ function showTictactoeModal() {
 // HOME
 $(".home-link").click(function () {
   event.preventDefault();
-  $('html,body').animate({
-    scrollTop: $(".landing-section").offset().top
-  },
-    'slow');
-  closeMobileNav()
+  $("html,body").animate(
+    {
+      scrollTop: $(".landing-section").offset().top,
+    },
+    "slow"
+  );
+  closeMobileNav();
 });
 
 // ABOUT ME
 $(".about-link").click(function () {
   event.preventDefault();
-  $('html,body').animate({
-    scrollTop: $(".bio-section").offset().top
-  },
-    'slow');
+  $("html,body").animate(
+    {
+      scrollTop: $(".bio-section").offset().top,
+    },
+    "slow"
+  );
   closeMobileNav();
-
 });
 
 // SKILLS
 $(".skills-link").click(function () {
   event.preventDefault();
-  $('html,body').animate({
-    scrollTop: $(".skills-section").offset().top
-  },
-    'slow');
+  $("html,body").animate(
+    {
+      scrollTop: $(".skills-section").offset().top,
+    },
+    "slow"
+  );
   closeMobileNav();
-
 });
 
 // PROJECTS
 $(".projects-link").click(function () {
   event.preventDefault();
-  $('html,body').animate({
-    scrollTop: $(".projects-section").offset().top
-  },
-    'slow');
+  $("html,body").animate(
+    {
+      scrollTop: $(".projects-section").offset().top,
+    },
+    "slow"
+  );
   closeMobileNav();
-
 });
 
 // FACTS
 $(".facts-link").click(function () {
   event.preventDefault();
-  $('html,body').animate({
-    scrollTop: $(".random-section").offset().top
-  },
-    'slow');
+  $("html,body").animate(
+    {
+      scrollTop: $(".random-section").offset().top,
+    },
+    "slow"
+  );
   closeMobileNav();
-
 });
 
 // ART
 $(".art-link").click(function () {
   event.preventDefault();
-  $('html,body').animate({
-    scrollTop: $(".artwork-section").offset().top
-  },
-    'slow');
+  $("html,body").animate(
+    {
+      scrollTop: $(".artwork-section").offset().top,
+    },
+    "slow"
+  );
   closeMobileNav();
-
 });
 
 // CONTACT
 $(".contact-link").click(function () {
   event.preventDefault();
-  $('html,body').animate({
-    scrollTop: $(".contact-section").offset().top
-  },
-    'slow');
+  $("html,body").animate(
+    {
+      scrollTop: $(".contact-section").offset().top,
+    },
+    "slow"
+  );
   closeMobileNav();
-
 });
-
-
-
 
 // Click Handlers
 function addHandlers() {
   // Modal
-  $('#exit-modal-button').on('click', closeModal);
-  $('.modal-div').on('click', closeModal);
+  $("#exit-modal-button").on("click", closeModal);
+  $(".modal-div").on("click", closeModal);
 
   // Cards
-  $('#pathfinder-card').on('click', showPathfinderModal);
-  $('#woofer-card').on('click', showWooferModal);
-  $('#facedetect-card').on('click', showFacedetectModal);
-  $('#curio-card').on('click', showCurioModal);
-  $('#tictactoe-card').on('click', showTictactoeModal);
+  $("#pathfinder-card").on("click", showPathfinderModal);
+  $("#woofer-card").on("click", showWooferModal);
+  $("#facedetect-card").on("click", showFacedetectModal);
+  $("#curio-card").on("click", showCurioModal);
+  $("#tictactoe-card").on("click", showTictactoeModal);
 
   // Nav
-  $('.nav-mobile-button').on('click', openMobileNav);
-  $('#close-menu-button').on('click', closeMobileNav);
-
+  $(".nav-mobile-button").on("click", openMobileNav);
+  $("#close-menu-button").on("click", closeMobileNav);
 }
 
 // On Site Load
 $(() => {
   addHandlers();
   onSiteLoad();
-})
+});
